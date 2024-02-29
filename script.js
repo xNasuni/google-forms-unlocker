db50fff59f2c5bd322174b64184747a9806a7e1d
Mia Rodriguez <62818119+xNasuni@users.noreply.github.com>
2024-02-28 21:56:37 -0500

    Update script.jsadgsdg

--- script.js
+++ script.js
@@ -11,6 +11,27 @@
         }
     }
 
+    function WaitForSelector(selector) { // source: https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+        return new Promise(resolve => {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+    
+            const observer = new MutationObserver(mutations => {
+                if (document.querySelector(selector)) {
+                    observer.disconnect();
+                    resolve(document.querySelector(selector));
+                }
+            });
+    
+            // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
+        });
+    }
+
     function AddButton(ButtonInfo, FormHolder) {
         for (var TextHolder of document.getElementsByClassName('NPEfkd RveJvd snByac')) {
             if (TextHolder.innerText === 'Continue') {
@@ -176,7 +197,7 @@
     var HeaderClass = 'kVkD5b'
     var FormClass = 'mGzJpd'
     
-    var Header = document.getElementsByClassName(HeaderClass)[0]
+    var Header = WaitForSelector(`.${HeaderClass}`)
     var Form = document.getElementsByClassName(FormClass)[0]
     
     if (Header === undefined) {
