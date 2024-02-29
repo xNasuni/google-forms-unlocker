3bd8fba7be9927dda664778d462580ae8f2ff8db
Mia Rodriguez <62818119+xNasuni@users.noreply.github.com>
2024-02-29 08:58:31 -0500

    Update script.userscript.js

--- script.userscript.js
+++ script.userscript.js
@@ -7,42 +7,44 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-let events_to_block = [
-  "visibilitychange",
-  "webkitvisibilitychange",
-  "mozvisibilitychange",
-  "hasFocus",
-  "blur",
-  "focus",
-  "mouseleave"
-]
- 
-for (event_name of events_to_block) {
-  document.addEventListener(event_name, function (event) {
-      event.preventDefault();
-      event.stopPropagation();
-      event.stopImmediatePropagation();
-  }, true);
-}
- 
-for (event_name of events_to_block) {
-  window.addEventListener(event_name, function (event) {
-      event.preventDefault();
-      event.stopPropagation();
-      event.stopImmediatePropagation();
-  }, true);
-}
- 
- 
-document.hasFocus = function () { return true; };
-document.onvisibilitychange = null;
-Object.defineProperty(document, "visibilityState", { value: "visible" });
-Object.defineProperty(document, "hidden", { value: false });
-Object.defineProperty(document, "mozHidden", { value: false });
-Object.defineProperty(document, "webkitHidden", { value: false });
-Object.defineProperty(document, "webkitVisibilityState", { value: "visible" });
-// the script above is https://greasyfork.org/en/scripts/427254-preventpagevisibility/code
-// bc iirc if you leave the tab you get kicked so like yeah
+
+Wnd.document.originalAEL = Wnd.document.addEventListener;
+
+var IntervalId = -1;
+IntervalId = setInterval(() => {
+    Wnd.Object.defineProperty(Wnd.document, 'hidden', {
+        value: false,
+        writable: false
+    })
+    Wnd.Object.defineProperty(Wnd.document, 'visibilityState', {
+        value: "visible",
+        writable: false
+    })
+    Wnd.Object.defineProperty(Wnd.document, 'webkitVisibilityState', {
+        value: "visible",
+        writable: false
+    })
+    Wnd.Object.defineProperty(Wnd.document, 'mozVisibilityState', {
+        value: "visible",
+        writable: false
+    })
+    Wnd.Object.defineProperty(Wnd.document, 'msVisibilityState', {
+        value: "visible",
+        writable: false
+    })
+    if (Wnd.document.originalAEL == undefined) {
+        Wnd.document.originalAEL = Wnd.document.addEventListener;
+    }
+    Wnd.document.addEventListener = function(eventType, method, options) {
+        Wnd.console.log(`type ${eventType} registered with`, method)
+        if (eventType == 'mozvisibilitychange' || eventType == 'webkitvisibilitychange' || eventType == 'msvisibilitychange' || eventType == 'visibilitychange') {
+            return;
+        } else {
+            return Wnd.document.originalAEL.apply(this, arguments)
+        }
+    }
+}, 100); // "anti" anti cheat incase anything gets added ig ill have to hide all these public vars
+// the stuff below is to let you change tabs and not let the window think you left and throw an exception bc it thinks ur 
 
 (async () => {
     if (window.LockedModeSpoof === true) {
