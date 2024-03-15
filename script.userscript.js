baa12a8f6632459187856ade6b538e8c7d6b101a
Mia Rodriguez <62818119+xNasuni@users.noreply.github.com>
2024-03-15 00:24:51 -0400

    fix for 1.1

--- script.userscript.js
+++ script.userscript.js
@@ -4,43 +4,45 @@
 // @description  unlocks google forms
 // @author       Mia
 // @match        *docs.google.com/*
+// @version      1.1
+// @downloadURL  https://github.com/xNasuni/google-forms-unlocker/raw/main/script.userscript.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-Wnd.document.originalAEL = Wnd.document.addEventListener;
+document.originalAEL = document.addEventListener;
 
 var IntervalId = -1;
 IntervalId = setInterval(() => {
-    Wnd.Object.defineProperty(Wnd.document, 'hidden', {
+    Object.defineProperty(document, 'hidden', {
         value: false,
         writable: false
     })
-    Wnd.Object.defineProperty(Wnd.document, 'visibilityState', {
+    Object.defineProperty(document, 'visibilityState', {
         value: "visible",
         writable: false
     })
-    Wnd.Object.defineProperty(Wnd.document, 'webkitVisibilityState', {
+    Object.defineProperty(document, 'webkitVisibilityState', {
         value: "visible",
         writable: false
     })
-    Wnd.Object.defineProperty(Wnd.document, 'mozVisibilityState', {
+    Object.defineProperty(document, 'mozVisibilityState', {
         value: "visible",
         writable: false
     })
-    Wnd.Object.defineProperty(Wnd.document, 'msVisibilityState', {
+    Object.defineProperty(document, 'msVisibilityState', {
         value: "visible",
         writable: false
     })
-    if (Wnd.document.originalAEL == undefined) {
-        Wnd.document.originalAEL = Wnd.document.addEventListener;
+    if (document.originalAEL == undefined) {
+        document.originalAEL = document.addEventListener;
     }
-    Wnd.document.addEventListener = function(eventType, method, options) {
-        Wnd.console.log(`type ${eventType} registered with`, method)
+    document.addEventListener = function(eventType, method, options) {
+        console.log(`type ${eventType} registered with`, method)
         if (eventType == 'mozvisibilitychange' || eventType == 'webkitvisibilitychange' || eventType == 'msvisibilitychange' || eventType == 'visibilitychange') {
             return;
         } else {
-            return Wnd.document.originalAEL.apply(this, arguments)
+            return document.originalAEL.apply(this, arguments)
         }
     }
 }, 100); // "anti" anti cheat incase anything gets added ig ill have to hide all these public vars
